ef9bb7fcb6a204cc4cf5f9deedf933a54fabf4f2
Gabriel B <gabelmo92@hotmail.com>
2020-01-24 18:44:07 +0100

    fixes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-// import { Provider } from 'react-redux'
-// import store from './redux/store'
-
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-
-const rootElement = document.getElementById('root')
-ReactDOM.render(
-    // <Provider store={store}>
-    <App />,
-  // </Provider>,
-  rootElement
-  )
-  
-  serviceWorker.unregister();
+serviceWorker.unregister();
